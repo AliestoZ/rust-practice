cbe437726a97a29a346216c94c1f6933681b688d
于洋 <yuyang.pkq@bytedance.com>
2024-06-26 21:44:09 +0800

    test

---
+++ demo002-data_type/target/debug/deps/demo002_data_type-b38e7e355278563e.d
@@ -0,0 +1,9 @@
+/Users/bytedance/RustProjects/RustDemos/demo002-data_type/target/debug/deps/libdemo002_data_type-b38e7e355278563e.rmeta: src/main.rs Cargo.toml
+
+/Users/bytedance/RustProjects/RustDemos/demo002-data_type/target/debug/deps/demo002_data_type-b38e7e355278563e.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
